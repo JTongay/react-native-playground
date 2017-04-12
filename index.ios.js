99128bb69c2c6e9ed992c739c7c0a4cdff79e5db
Joseph Tongay <dirk.tongay@gmail.com>
2017-04-11 20:45:45 -0500

    list view is a pain in the ass

--- index.ios.js
+++ index.ios.js
@@ -3,22 +3,74 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView
 } from 'react-native';
 
 import Layout from './src/pages/layout'
 import ViewContainer from './src/components/ViewContainer'
 import StatusBarBackground from './src/components/StatusBarBackground'
 
-export default class testapp extends Component {
+const people = [
+  {
+    firstName: 'Joey',
+    lastName: 'Tongay',
+    age: 27
+  },
+  {
+    firstName: 'Erin',
+    lastName: 'Miller',
+    age: 28
+  },
+  {
+    firstName: 'Beans',
+    lastName: 'Vinny',
+    age: 4
+  }
+]
+
+export default class testapp extends React.Component {
+
+  constructor(props){
+    super(props)
+    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2})
+    this.state = {
+      peopleDataSource: ds.cloneWithRows(people)
+    }
+  }
+
   render() {
+
     return (
       <ViewContainer>
         <StatusBarBackground backgroundColor={"skyblue"} />
         <Text style={ {textAlign: "center"} }>Booyah</Text>
+        <ListView
+          style = {{marginTop: 100}}
+          dataSource = {this.state.peopleDataSource}
+          renderRow = {(person)=> { return this._renderPersonRow(person) }}
+           />
       </ViewContainer>
     );
   }
+
+  _renderPersonRow(person){
+    return (
+      <View style={styles.personRow}>
+        <Text style={styles.personName}>{person.firstName}</Text>
+      </View>
+    )
+  }
+
 }
+
+const styles = StyleSheet.create({
+  personRow: {
+    justifyContent: "center"
+  },
+  personName: {
+
+  }
+})
 
 AppRegistry.registerComponent('testapp', () => testapp);
