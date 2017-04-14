c4d07aa0dd69b146f5c784a1194ec78b94a301de
Joseph Tongay <dirk.tongay@gmail.com>
2017-04-13 23:28:41 -0500

    setting up navigator

--- index.ios.js
+++ index.ios.js
@@ -2,88 +2,24 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View,
-  ListView,
-  TouchableOpacity
+  Navigator
 } from 'react-native';
 import _ from 'lodash'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
-import Layout from './src/pages/layout'
-import ViewContainer from './src/components/ViewContainer'
-import StatusBarBackground from './src/components/StatusBarBackground'
-
-const people = [
-  {
-    firstName: 'Joey',
-    lastName: 'Tongay',
-    age: 27
-  },
-  {
-    firstName: 'Erin',
-    lastName: 'Miller',
-    age: 28
-  },
-  {
-    firstName: 'Beans',
-    lastName: 'Vinny',
-    age: 4
-  }
-]
+import PeopleIndexScreen from './src/pages/PeopleIndexScreen'
 
 export default class testapp extends React.Component {
 
-  constructor(props){
-    super(props)
-    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2})
-    this.state = {
-      peopleDataSource: ds.cloneWithRows(people)
-    }
-  }
-
   render() {
-
     return (
-      <ViewContainer>
-        <StatusBarBackground backgroundColor={"skyblue"} />
-        <Text style={ {textAlign: "center"} }>Booyah</Text>
-        <ListView
-          style = {{marginTop: 100}}
-          dataSource = {this.state.peopleDataSource}
-          renderRow = {(person)=> { return this._renderPersonRow(person) }}
-           />
-      </ViewContainer>
+      <Navigator />
     );
   }
-
-  _renderPersonRow(person){
-    return (
-      <TouchableOpacity style={styles.personRow} onPress={(event)=> console.log(person)}>
-        <Text style={styles.personName}>{`${_.capitalize(person.firstName)} ${_.capitalize(person.lastName)}`}</Text>
-        <View style={{flex: 1}} />
-        <Icon name="chevron-right" style={styles.personMoreIcon} />
-      </TouchableOpacity>
-    )
-  }
-
 }
 
 const styles = StyleSheet.create({
-  personRow: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    height: 50
-  },
-  personName: {
-    marginLeft: 25
-  },
-  personMoreIcon: {
-    color: "green",
-    height: 20,
-    width: 20,
-    marginRight: 25
-  }
+
 })
 
 AppRegistry.registerComponent('testapp', () => testapp);
---
+++ src/pages/PeopleIndexScreen.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  TouchableOpacity
+} from 'react-native';
+import _ from 'lodash'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import ViewContainer from '../components/ViewContainer'
+import StatusBarBackground from '../components/StatusBarBackground'
+
+const people = [
+  {
+    firstName: 'Joey',
+    lastName: 'Tongay',
+    age: 27
+  },
+  {
+    firstName: 'Erin',
+    lastName: 'Miller',
+    age: 28
+  },
+  {
+    firstName: 'Beans',
+    lastName: 'Vinny',
+    age: 4
+  }
+]
+
+export default class PeopleIndexScreen extends React.Component {
+
+  constructor(props){
+    super(props)
+    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2})
+    this.state = {
+      peopleDataSource: ds.cloneWithRows(people)
+    }
+  }
+
+  render() {
+
+    return (
+      <ViewContainer>
+        <StatusBarBackground backgroundColor={"skyblue"} />
+        <Text style={ {textAlign: "center"} }>Booyah</Text>
+        <ListView
+          style = {{marginTop: 100}}
+          dataSource = {this.state.peopleDataSource}
+          renderRow = {(person)=> { return this._renderPersonRow(person) }}
+           />
+      </ViewContainer>
+    );
+  }
+
+  _renderPersonRow(person){
+    return (
+      <TouchableOpacity style={styles.personRow} onPress={(event)=> console.log(person)}>
+        <Text style={styles.personName}>{`${_.capitalize(person.firstName)} ${_.capitalize(person.lastName)}`}</Text>
+        <View style={{flex: 1}} />
+        <Icon name="chevron-right" style={styles.personMoreIcon} />
+      </TouchableOpacity>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  personRow: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    height: 50
+  },
+  personName: {
+    marginLeft: 25
+  },
+  personMoreIcon: {
+    color: "green",
+    height: 20,
+    width: 20,
+    marginRight: 25
+  }
+})
